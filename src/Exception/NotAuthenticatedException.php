8fbba34b588ed8753668d6aaedbe0a6eac64b0a1
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-12-02 17:37:38 +0100

    add exception NotAuthenticatedException

---
+++ src/Exception/NotAuthenticatedException.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Gobline Framework
+ *
+ * (c) Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gobline\Auth\Exception;
+
+/**
+ * @author Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ */
+class NotAuthenticatedException extends \RuntimeException 
+{
+}
